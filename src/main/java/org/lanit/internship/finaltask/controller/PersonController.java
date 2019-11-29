447f32816736ddf24652808b47c26ea31d4e3417
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-29 11:21:58 +0500

    Application10Commit

--- src/main/java/org/lanit/internship/finaltask/controller/PersonController.java
+++ src/main/java/org/lanit/internship/finaltask/controller/PersonController.java
@@ -1,9 +1,7 @@
 package org.lanit.internship.finaltask.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.lanit.internship.finaltask.exceptions.NotFoundException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,7 +11,8 @@ import java.util.Map;
 @RestController
 @RequestMapping("person")
 public class PersonController {
-    public List<Map<String,String>> persons = new ArrayList<Map<String,String>>() {{
+    private int counter = 4;
+    private List<Map<String,String>> persons = new ArrayList<Map<String,String>>() {{
         add(new HashMap<String, String>() {{put("id","1"); put("name", "Ivan"); put("birthdate","2000-05-25");}});
         add(new HashMap<String, String>() {{put("id","2"); put("name", "Pavel"); put("birthdate","1989-01-17");}});
         add(new HashMap<String, String>() {{put("id","3"); put("name", "Anton"); put("birthdate","1970-03-06");}});
@@ -29,7 +28,16 @@ public class PersonController {
         return persons.stream()
                 .filter(person -> person.get("id").equals(id))
                 .findFirst()
-                .orElseThrow();
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String,String> create(@RequestBody Map<String,String> person) {
+        person.put("id", String.valueOf(counter++));
+
+        persons.add(person);
+
+        return person;
     }
 
 }
