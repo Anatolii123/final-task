b50495c3e9c5d29174ad0b37ebc7490b252c2e77
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-29 11:46:15 +0500

    Application14Commit

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,16 @@
+Vue.component('persons-list', {
+    props: ['persons'],
+    template: '<div><div v-for="person in persons">{{person.name}} {{person.birthdate}}</div></div>'
+})
+
 var app = new Vue({
     el: '#app',
+    template: '<persons-list :persons="persons" />',
     data: {
-        message: 'Hello, user!'
+        persons: [
+            {id: '1', name: 'A', birthdate: '1990-05-05'},
+            {id: '2', name: 'B', birthdate: '1990-05-05'},
+            {id: '3', name: 'C', birthdate: '1990-05-05'},
+        ]
     }
 })
