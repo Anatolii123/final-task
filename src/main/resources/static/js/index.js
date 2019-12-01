0cfdc990df03d1f94156a248ce7b8dc1a702bd23
AKusakin <anatolii.kusakin1234@gmail.com>
2019-12-01 22:30:41 +0500

    Application56Commit

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,8 +1,7 @@
-var id = document.getElementById("personId");
-var xhr = new XMLHttpRequest();
-var param = 'personid=' + id;
-
 function sendRequest() {
+    var id = document.getElementById("personId");
+    var xhr = new XMLHttpRequest();
+    var param = 'personid=' + id;
     xhr.open("GET", 'http://localhost:8080/submit?' + param, true);
     xhr.send();
 }
