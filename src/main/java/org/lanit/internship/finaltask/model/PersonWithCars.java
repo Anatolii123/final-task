7938484698eb15bcff1327a4faac68e52d9ae2c1
AKusakin <anatolii.kusakin1234@gmail.com>
2019-12-02 23:13:10 +0500

    Application80Commit

--- src/main/java/org/lanit/internship/finaltask/model/PersonWithCars.java
+++ src/main/java/org/lanit/internship/finaltask/model/PersonWithCars.java
@@ -2,7 +2,6 @@ package org.lanit.internship.finaltask.model;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class PersonWithCars extends Person {
     private List<Optional<Car>> cars;
