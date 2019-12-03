13f45fe5d651ee33558a7e8eba1b9c55428371c7
AKusakin <anatolii.kusakin1234@gmail.com>
2019-12-03 16:47:49 +0500

    Application102Commit

--- src/test/java/org/lanit/internship/finaltask/controller/PersonCarControllerTest.java
+++ src/test/java/org/lanit/internship/finaltask/controller/PersonCarControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,11 +31,28 @@ class PersonCarControllerTest {
     }
 
     @Test
-    void savePerson() {
+    void savePerson() throws Exception {
+        this.mockMvc.perform(post("/person")
+                .header("Content-Type","application/json").content("{\n" +
+                        "    \"id\":4,\n" +
+                        "    \"name\":\"Nads\",\n" +
+                        "    \"birthDate\":\"2006-05-25\"\n" +
+                        "}"))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
-    void saveCar() {
+    void saveCar() throws Exception {
+        this.mockMvc.perform(post("/car")
+                .header("Content-Type","application/json").content("{\n" +
+                        "    \"id\":4,\n" +
+                        "    \"model\":\"VAZ-Lada Kalina\",\n" +
+                        "    \"horsepower\":395,\n" +
+                        "    \"ownerId\":1\n" +
+                        "}"))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
@@ -43,13 +61,14 @@ class PersonCarControllerTest {
 
     @Test
     void getStatistics() throws Exception {
-        this.mockMvc.perform(get("http://localhost:8080/statistics"))
+        this.mockMvc.perform(get("/statistics"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().json("{\"personcount\":4,\"carcount\":4,\"uniquevendorcount\":3}"));
+                .andExpect(content().json("{\"personcount\":0,\"carcount\":0,\"uniquevendorcount\":0}"));
     }
 
     @Test
     void clearDB() {
+
     }
 }
