0ea31bf79899b3272ee9aaccf009bd71e769da6a
AKusakin <anatolii.kusakin1234@gmail.com>
2019-12-03 17:21:43 +0500

    Application103Commit

--- src/test/java/org/lanit/internship/finaltask/controller/PersonCarControllerTest.java
+++ src/test/java/org/lanit/internship/finaltask/controller/PersonCarControllerTest.java
@@ -1,15 +1,21 @@
 package org.lanit.internship.finaltask.controller;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.lanit.internship.finaltask.model.Car;
+import org.lanit.internship.finaltask.model.CarRepo;
+import org.lanit.internship.finaltask.model.Person;
+import org.lanit.internship.finaltask.model.PersonRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
+import javax.annotation.Resource;
+import java.sql.Date;
+import java.util.Optional;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,37 +28,53 @@ class PersonCarControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Resource
+    private CarRepo carRepo;
+
+    @Resource
+    private PersonRepo personRepo;
+
     @Test
-    void personsList() {
+    void personsList() throws Exception {
+        this.mockMvc.perform(get("/persons"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
     }
 
     @Test
-    void carsList() {
+    void carsList() throws Exception {
+        this.mockMvc.perform(get("/cars"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
     }
 
     @Test
     void savePerson() throws Exception {
-        this.mockMvc.perform(post("/person")
-                .header("Content-Type","application/json").content("{\n" +
-                        "    \"id\":4,\n" +
-                        "    \"name\":\"Nads\",\n" +
-                        "    \"birthDate\":\"2006-05-25\"\n" +
-                        "}"))
-                .andDo(print())
-                .andExpect(status().isOk());
+        Person person = new Person();
+        person.setId(1L);
+        person.setName("Name");
+        person.setBirthDate(Date.valueOf("2000-05-25"));
+        personRepo.save(person);
+        Optional<Person> person2 =  personRepo.findById(1L);
+        Assert.assertEquals(person.getId(),person2.get().getId());
+        Assert.assertEquals(person.getName(),person2.get().getName());
+        Assert.assertEquals(person.getBirthDate(),person2.get().getBirthDate());
     }
 
     @Test
     void saveCar() throws Exception {
-        this.mockMvc.perform(post("/car")
-                .header("Content-Type","application/json").content("{\n" +
-                        "    \"id\":4,\n" +
-                        "    \"model\":\"VAZ-Lada Kalina\",\n" +
-                        "    \"horsepower\":395,\n" +
-                        "    \"ownerId\":1\n" +
-                        "}"))
-                .andDo(print())
-                .andExpect(status().isOk());
+        Car car = new Car();
+        car.setId(1L);
+        car.setModel("VAZ-Lada");
+        car.setHorsepower(380);
+        car.setOwnerId(1L);
+        carRepo.save(car);
+        Optional<Car> car2 =  carRepo.findById(2L);
+        Assert.assertEquals(car.getModel(),car2.get().getModel());
+        Assert.assertEquals(car.getHorsepower(),car2.get().getHorsepower());
+        Assert.assertEquals(car.getOwnerId(),car2.get().getOwnerId());
     }
 
     @Test
