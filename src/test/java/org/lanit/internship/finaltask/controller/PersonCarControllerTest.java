3622c12039b61947b157aa75bc0f11d2cb1bda0e
AKusakin <anatolii.kusakin1234@gmail.com>
2019-12-03 18:13:43 +0500

    Application105Commit

--- src/test/java/org/lanit/internship/finaltask/controller/PersonCarControllerTest.java
+++ src/test/java/org/lanit/internship/finaltask/controller/PersonCarControllerTest.java
@@ -1,6 +1,8 @@
 package org.lanit.internship.finaltask.controller;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.lanit.internship.finaltask.model.Car;
@@ -34,6 +36,29 @@ class PersonCarControllerTest {
     @Resource
     private PersonRepo personRepo;
 
+    public void savePersonMethod() throws Exception {
+        Person person = new Person();
+        person.setId(1L);
+        person.setName("Name");
+        person.setBirthDate(Date.valueOf("2000-05-25"));
+        personRepo.save(person);
+    }
+
+    public void saveCarMethod() {
+        Car car = new Car();
+        car.setId(1L);
+        car.setModel("VAZ-Lada");
+        car.setHorsepower(380);
+        car.setOwnerId(1L);
+        carRepo.save(car);
+    }
+
+    public void clearDatabase() throws Exception {
+        this.mockMvc.perform(get("/clear"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
     @Test
     void personsList() throws Exception {
         this.mockMvc.perform(get("/persons"))
@@ -47,34 +72,25 @@ class PersonCarControllerTest {
         this.mockMvc.perform(get("/cars"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().json("[]"));
+                .andExpect(content().json("[{\"id\":2,\"model\":\"VAZ-Lada\",\"horsepower\":380,\"ownerId\":1,\"vendorModel\":\"VAZ\",\"modelModel\":\"Lada\"}]"));
     }
 
     @Test
     void savePerson() throws Exception {
-        Person person = new Person();
-        person.setId(1L);
-        person.setName("Name");
-        person.setBirthDate(Date.valueOf("2000-05-25"));
-        personRepo.save(person);
-        Optional<Person> person2 =  personRepo.findById(1L);
-        Assert.assertEquals(person.getId(),person2.get().getId());
-        Assert.assertEquals(person.getName(),person2.get().getName());
-        Assert.assertEquals(person.getBirthDate(),person2.get().getBirthDate());
+        savePersonMethod();
+        Optional<Person> person2 =  personRepo.findById(4L);
+        Assert.assertEquals(Long.valueOf(4L),person2.get().getId());
+        Assert.assertEquals("Name",person2.get().getName());
+        Assert.assertEquals(Date.valueOf("2000-05-25"),person2.get().getBirthDate());
     }
 
     @Test
     void saveCar() throws Exception {
-        Car car = new Car();
-        car.setId(1L);
-        car.setModel("VAZ-Lada");
-        car.setHorsepower(380);
-        car.setOwnerId(1L);
-        carRepo.save(car);
-        Optional<Car> car2 =  carRepo.findById(2L);
-        Assert.assertEquals(car.getModel(),car2.get().getModel());
-        Assert.assertEquals(car.getHorsepower(),car2.get().getHorsepower());
-        Assert.assertEquals(car.getOwnerId(),car2.get().getOwnerId());
+        saveCarMethod();
+        Optional<Car> car2 =  carRepo.findById(5L);
+        Assert.assertEquals("VAZ-Lada",car2.get().getModel());
+        Assert.assertEquals(Integer.valueOf(380),car2.get().getHorsepower());
+        Assert.assertEquals(Long.valueOf(1L),car2.get().getOwnerId());
     }
 
     @Test
@@ -84,14 +100,18 @@ class PersonCarControllerTest {
 
     @Test
     void getStatistics() throws Exception {
+        savePersonMethod();
+        saveCarMethod();
         this.mockMvc.perform(get("/statistics"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().json("{\"personcount\":0,\"carcount\":0,\"uniquevendorcount\":0}"));
+                .andExpect(content().json("{\"personcount\":1,\"carcount\":1,\"uniquevendorcount\":1}"));
     }
 
     @Test
-    void clearDB() {
-
+    void clearDB() throws Exception {
+        savePersonMethod();
+        saveCarMethod();
+        clearDatabase();
     }
 }
