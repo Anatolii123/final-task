94431d204d62c3c0bc4acc5ad40d551bc498f07d
AKusakin <anatolii.kusakin1234@gmail.com>
2019-12-03 11:57:06 +0500

    Application88Commit

--- src/main/java/org/lanit/internship/finaltask/service/PersonCarService2.java
+++
@@ -1,9 +0,0 @@
-package org.lanit.internship.finaltask.service;
-
-import org.lanit.internship.finaltask.model.Car;
-import org.lanit.internship.finaltask.model.Person;
-
-public interface PersonCarService2 {
-    Person save(Person person);
-    Car save(Car car);
-}
