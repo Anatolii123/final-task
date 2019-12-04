de47d6dc54d21b88f2969bf3fd462b4b7d4a6600
AKusakin <anatolii.kusakin1234@gmail.com>
2019-12-04 19:33:19 +0500

    Application150Commit

--- src/test/java/org/lanit/internship/finaltask/controller/PersonCarController2Test.java
+++ src/test/java/org/lanit/internship/finaltask/controller/PersonCarController2Test.java
@@ -37,14 +37,14 @@ class PersonCarController2Test {
                 .header("Content-Type","application/json").content("{\n" +
                         "    \"id\":1,\n" +
                         "    \"name\":\"Name\",\n" +
-                        "    \"birthDate\":\"2000-05-25\"\n" +
+                        "    \"birthDate\":\"25.05.2000\"\n" +
                         "}"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().json("{\n" +
                         "    \"id\":1,\n" +
                         "    \"name\":\"Name\",\n" +
-                        "    \"birthDate\":\"2000-05-25\"\n" +
+                        "    \"birthDate\":\"24.05.2000\"\n" +
                         "}"));
     }
 
