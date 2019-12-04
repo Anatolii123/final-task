a44b5a2106beea3c872404f6db0da1394fa6ff3a
AKusakin <anatolii.kusakin1234@gmail.com>
2019-12-04 16:24:52 +0500

    Application138Commit

--- src/test/java/org/lanit/internship/finaltask/service/PersonCarServiceImplTest.java
+++ src/test/java/org/lanit/internship/finaltask/service/PersonCarServiceImplTest.java
@@ -175,7 +175,9 @@ class PersonCarServiceImplTest {
         Assert.assertEquals(0, personCarService.findAllPersons().size());
     }
 
-    //checkWrongExecution
+    /**
+     *WrongExecutionTests
+     */
 
     @Test
     void personIsInvalidBadRequest() throws Exception {
