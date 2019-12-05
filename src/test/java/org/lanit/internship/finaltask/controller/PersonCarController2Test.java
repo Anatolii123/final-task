33b467d8c777216da74d086f23497409bb07e1e8
AKusakin <anatolii.kusakin1234@gmail.com>
2019-12-05 13:49:59 +0500

    Application169Commit

--- src/test/java/org/lanit/internship/finaltask/controller/PersonCarController2Test.java
+++ src/test/java/org/lanit/internship/finaltask/controller/PersonCarController2Test.java
@@ -1,5 +1,6 @@
 package org.lanit.internship.finaltask.controller;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -21,14 +24,25 @@ class PersonCarController2Test {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void savePerson() throws Exception {
-        this.mockMvc.perform(post("/2/person")
+    @AfterEach
+    void clearDB() throws Exception {
+        this.mockMvc.perform(get("/clear"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    public ResultActions personSave() throws Exception {
+         return this.mockMvc.perform(post("/2/person")
                 .header("Content-Type","application/json").content("{\n" +
                         "    \"id\":1,\n" +
                         "    \"name\":\"Name\",\n" +
                         "    \"birthDate\":\"25.05.2000\"\n" +
-                        "}"))
+                        "}"));
+    }
+
+    @Test
+    void savePerson() throws Exception {
+        personSave()
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().json("{\n" +
@@ -40,6 +54,7 @@ class PersonCarController2Test {
 
     @Test
     void saveCar() throws Exception {
+        personSave();
         this.mockMvc.perform(post("/2/car")
                 .header("Content-Type","application/json").content("{\n" +
                         "    \"id\":2,\n" +
@@ -75,6 +90,7 @@ class PersonCarController2Test {
 
     @Test
     void saveCarBadRequest() throws Exception {
+        personSave();
         this.mockMvc.perform(post("/2/car")
                 .header("Content-Type","application/json").content("{\n" +
                         "    \"id\":2,\n" +
